71441383fd717338dafa8eabff3103b51a451f18
Gagana M R <gaganamr710@gmail.com>
2026-04-03 18:50:17 +0530

    Integrate Qdrant client and define collections
    Added Qdrant client initialization and collection creation function.

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,27 @@
-import os
-from dotenv import load_dotenv
+from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance
+from app.config import QDRANT_URL, QDRANT_API_KEY
+from qdrant_client import QdrantClient
 
-load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+client = QdrantClient(
+    url=QDRANT_URL.strip(),   # 🔥 removes hidden spaces
+    api_key=QDRANT_API_KEY,
+    timeout=120,
+    check_compatibility=False
+)
+
+def create_collections():
+
+    collections = ["mahabharata", "ramayana", "puranas"]
+
+    for col in collections:
+
+        if not client.collection_exists(col):
+            client.create_collection(
+                collection_name=col,
+                vectors_config=VectorParams(
+                    size=384,
+                    distance=Distance.COSINE
+                )
+            )
